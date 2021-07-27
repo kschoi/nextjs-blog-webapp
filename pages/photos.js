b0b07dcc0bb204ab5615115d7e995e9b53a8ce10
kyungsun choi <kschoi0308@gmail.com>
2021-07-27 13:46:51 +0900

    finish tutorial

--- pages/photos.js
+++ pages/photos.js
@@ -1,21 +1,28 @@
 import React from "react";
 import HeadInfo from "../components/HeadInfo";
 import Image from "next/image";
+import photosStyles from "../styles/Photos.module.css";
+import Link from "next/link";
 
 const photos = ({ photos }) => {
   return (
     <div>
       <HeadInfo title="My Blog: Photos" />
       <h1>My Photos</h1>
-      <ul>
+      <ul className={photosStyles.photos}>
         {photos.map((photo) => (
           <li key={photo.id}>
-            <Image
-              src={photo.thumbnailUrl}
-              width={100}
-              height={100}
-              alt={photo.title}
-            />
+            <Link href={`/photos/${photo.id}`}>
+              <a>
+                <Image
+                  src={photo.thumbnailUrl}
+                  width={100}
+                  height={100}
+                  alt={photo.title}
+                />
+                <span>{photo.title}</span>
+              </a>
+            </Link>
           </li>
         ))}
       </ul>
